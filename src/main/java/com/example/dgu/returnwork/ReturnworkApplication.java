049ac27782fe09448c3d370f87d0fdd76e0704fd
galllee <kimim0410@gmail.com>
2025-06-22 17:14:47 +0900

    feat: 스프링 기본 설정

---
+++ src/main/java/com/example/dgu/returnwork/ReturnworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.dgu.returnwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReturnworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReturnworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dgu/returnwork/ReturnworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dgu.returnwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReturnworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
