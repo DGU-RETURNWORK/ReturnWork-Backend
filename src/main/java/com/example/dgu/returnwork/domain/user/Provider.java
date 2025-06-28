7df719dba9e295c6bfdd38689006338cef1c112f
oOccasio <dhzktldh@gmail.com>
2025-06-28 21:30:11 +0900

    feat:User, Region 엔티티 구현

---
+++ src/main/java/com/example/dgu/returnwork/domain/user/Provider.java
@@ -0,0 +1,5 @@
+package com.example.dgu.returnwork.domain.user;
+
+public enum Provider {
+    GOOGLE, NORMAL
+}
---
+++ src/main/java/com/example/dgu/returnwork/domain/user/Status.java
@@ -0,0 +1,5 @@
+package com.example.dgu.returnwork.domain.user;
+
+public enum Status {
+    ACTIVE,INACTIVE,DELETED
+}
