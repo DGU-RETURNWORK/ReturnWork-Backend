e60bb3d1bfd9cdeae24d04ef0cc363a941524727
oOccasio <dhzktldh@gmail.com>
2025-07-01 02:34:01 +0900

    feat:jwt 기본 설정

---
+++ src/main/java/com/example/dgu/returnwork/global/jwt/JwtTokenProvider.java
@@ -0,0 +1,111 @@
+package com.example.dgu.returnwork.global.jwt;
+
+import com.example.dgu.returnwork.global.properties.JwtProperties;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import java.security.Key;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class JwtTokenProvider{
+
+
+    private final JwtProperties jwtProperties;
+
+
+    private Key getSigningKey(){
+        return Keys.hmacShaKeyFor(jwtProperties.getSecretKey().getBytes());
+    }
+
+
+    public String generateAccessToken(Authentication authentication){
+        String email = authentication.getName();
+        Date now = new Date();
+        Date expiration = new Date(now.getTime() + jwtProperties.getExpiration().getAccess());
+
+
+        return Jwts.builder()
+                .setSubject(email)
+                .claim("role", authentication.getAuthorities().iterator().next().getAuthority())
+                .setIssuedAt(now)
+                .setExpiration(expiration)
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+
+    public String generateRefreshToken(Authentication authentication){
+        String email = authentication.getName();
+        Date now = new Date();
+        Date expiration = new Date(now.getTime() + jwtProperties.getExpiration().getRefresh());
+
+        return Jwts.builder()
+                .setSubject(email)
+                .claim("role", "REFRESH")
+                .setIssuedAt(now)
+                .setExpiration(expiration)
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+
+    }
+
+
+    public boolean validateToken(String token){
+        try{
+            Jwts.parserBuilder()
+                    .setSigningKey(getSigningKey())
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        }catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+
+    public Authentication getAuthentication(String token){
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        String email = claims.getSubject();
+        String role = claims.get("role", String.class);
+
+        User principal = new User(email, "", List.of(new SimpleGrantedAuthority(role)));
+        return new UsernamePasswordAuthenticationToken(principal, token, principal.getAuthorities());
+    }
+
+    public String resolveToken(HttpServletRequest request){
+        String bearerToken = request.getHeader(jwtProperties.getHeader());
+        if(StringUtils.hasText(bearerToken) && bearerToken.startsWith(jwtProperties.getPrefix())){
+            return bearerToken.substring(jwtProperties.getPrefix().length());
+        }
+        return null;
+    }
+
+    public Authentication extractAuthentication(HttpServletRequest request){
+        String accessToken = resolveToken(request);
+        if (accessToken == null || !validateToken(accessToken)){
+            throw new IllegalArgumentException("Invalid access token"); // 나중에 수정 필요
+        }
+
+        return getAuthentication(accessToken);
+    }
+
+}
