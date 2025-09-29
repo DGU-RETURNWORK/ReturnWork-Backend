8e7e88a2c2ff770a46e040a74fa938ffcc222111
oOccasio <dhzktldh@gmail.com>
2025-09-29 22:14:56 +0900

    [feat] #62 add S3 Config

--- src/main/java/com/example/dgu/returnwork/domain/user/User.java
+++ src/main/java/com/example/dgu/returnwork/domain/user/User.java
@@ -78,6 +78,9 @@ public class User extends BaseTimeEntity {
     @Column(name = "deleted_at")
     private LocalDateTime deletedAt;
 
+    @Column(name = "image_key")
+    private String imageKey;
+
     // == 양방향 관계 + CASCADE 설정 == //
     @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE, orphanRemoval = true, fetch = FetchType.LAZY)
     @Builder.Default
---
+++ src/main/java/com/example/dgu/returnwork/global/config/S3Config.java
@@ -0,0 +1,45 @@
+package com.example.dgu.returnwork.global.config;
+
+import com.example.dgu.returnwork.global.properties.S3Properties;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+
+@Configuration
+@RequiredArgsConstructor
+@Profile("s3")
+public class S3Config {
+
+    private final S3Properties s3Properties;
+
+    @Bean
+    public S3Client s3Client() {
+        AwsBasicCredentials credentials = AwsBasicCredentials.create(
+                s3Properties.getAccessKey(),
+                s3Properties.getSecretKey()
+        );
+        return S3Client.builder()
+                .region(Region.of(s3Properties.getRegion()))
+                .credentialsProvider(StaticCredentialsProvider.create(credentials))
+                .build();
+    }
+
+    @Bean
+    public S3Presigner s3Presigner() {
+        AwsBasicCredentials credentials = AwsBasicCredentials.create(
+                s3Properties.getAccessKey(),
+                s3Properties.getSecretKey()
+        );
+
+        return S3Presigner.builder()
+                .region(Region.of(s3Properties.getRegion()))
+                .credentialsProvider(StaticCredentialsProvider.create(credentials))
+                .build();
+    }
+}
