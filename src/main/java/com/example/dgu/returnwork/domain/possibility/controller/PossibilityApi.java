f2c85f50089236e2ddd19981e8ac0e43fbd3e6f9
galllee <kimim0410@gmail.com>
2025-09-29 19:09:00 +0900

    [dosc] #55 스웨거 ApiResponse 수정

--- src/main/java/com/example/dgu/returnwork/domain/possibility/controller/PossibilityApi.java
+++ src/main/java/com/example/dgu/returnwork/domain/possibility/controller/PossibilityApi.java
@@ -42,20 +42,36 @@ public interface PossibilityApi {
                                                     "jobFitness": 85,
                                                     "jobCode": "01010101"
                                                   },
-                                                  {
-                                                    "jobName": "전기 기기 조작원",
-                                                    "jobFitness": 75,
-                                                    "jobCode": "01010102"
-                                                  },
+                                                  ...,
                                                   {
                                                     "jobName": "물류 창고 관리자",
                                                     "jobFitness": 70,
                                                     "jobCode": "01010103"
                                                   }
                                                 ],
+                                                "jobDetails": [
+                                                          {
+                                                            "jobType": "법률안전",
+                                                            "jobName": "소방시설 유지보수",
+                                                            "imgUrl": "imageurl",
+                                                            "jobFitness": 80,
+                                                            "jobCode": "0502010410",
+                                                            "description": "소방시설 유지보수는 신체적 요구가 낮고 사고 후 복귀에 적합합니다. 이 직무는 주로 장비 점검 및 관리에 중점을 두어 안전하게 수행할 수 있습니다."
+                                                          },
+                                                          ...,
+                                                          {
+                                                            "jobType": "운전운송",
+                                                            "jobName": "정보통신망 설비 유지보수",
+                                                            "imgUrl": "imageurl",
+                                                            "jobFitness": 60,
+                                                            "jobCode": "0902020414",
+                                                            "description": "정보통신망 설비 유지보수는 주로 장비 점검 및 소프트웨어 관리 업무입니다. 사고 후에도 신체적 요구가 낮아 적합한 직무입니다."
+                                                          }
+                                                ],
                                                 "capabilities": [
                                                   "세밀한 손작업 필요 직무",
                                                   "고정밀/조립 제작 작업",
+                                                  ...,
                                                   "중량물 취급이 많은 직무"
                                                 ]
                                               }
